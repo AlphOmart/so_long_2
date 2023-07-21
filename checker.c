e645c89c11cac3d918de497451423241a7744dc0
AlphomArt <menelik.wubneh@gmail.com>
2023-07-21 21:07:04 +0200

    test

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 20:57:11 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/21 21:06:07 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/21 21:06:50 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,7 @@ static void	set_objs(t_data *data)
 
 void	is_valid(t_data *data, int size)
 {
+	(void)size;
 	set_objs(data);
 	//ft_is_open(data, size - 1);
 	//check_content();
