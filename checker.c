665cf2c17de1a5d0ece96d6adc52068bec90650a
AlphomArt <menelik.wubneh@gmail.com>
2023-07-21 22:13:16 +0200

    test

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 20:57:11 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/21 22:11:53 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/21 22:13:07 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,21 @@ void	ft_is_open(t_data *data, int size)
 	int	y;
 	int	x;
 
-//	x = ft_strlen(data->map_cpy[0]) - 2;
-//	while (data->map_cpy[0][x] && data->map_cpy[size - 1][x] && x > 0)
-//	{
-//		if (data->map_cpy[0][x] != data->object->wall ||
-//			data->map_cpy[size - 1][x] != data->object->wall)
-//			return (ft_error("map not closed\n", data, 2));
-//		x--;
-//	}
-	printf("%i\n", size - 1);
+	x = ft_strlen(data->map_cpy[0]) - 2;
+	while (data->map_cpy[0][x] && data->map_cpy[size - 1][x] && x > 0)
+	{
+		if (data->map_cpy[0][x] != data->object->wall ||
+			data->map_cpy[size - 1][x] != data->object->wall)
+			return (ft_error("map not closed\n", data, 2));
+		x--;
+	}
 	y = -1;
 	x = ft_strlen(data->map_cpy[0]) - 2;
 	printf("%i\n", x);
-	while (++y <= size) {
+	while (++y <= size -1)
 		if (data->map_cpy[y][0] != data->object->wall
 			|| data->map_cpy[y][x] != data->object->wall)
 			ft_error("Error : map not closed 2\n", data, 2);
-		printf("y = %i '%c'\n", y, data->map_cpy[y][0]);
-		printf("y = %i '%c'\n", y, data->map_cpy[y][x]);
-	}
 }
 
 static void	set_objs(t_data *data)
