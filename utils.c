1c413f15ae1cf4a4af5f8d8fe546d0158cece300
AlphomArt <menelik.wubneh@gmail.com>
2023-07-22 20:16:14 +0200

    test

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 20:13:05 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/21 23:30:51 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/22 20:16:06 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,6 @@ void	ft_free(t_data *data, int n)
 void	ft_error(char *str, t_data *data, int n)
 {
 	ft_free(data, n);
-	write(2, str, ft_strlen(str));
+	(void)write(2, str, ft_strlen(str));
 	exit(-1);
 }
