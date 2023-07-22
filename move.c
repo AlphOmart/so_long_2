98e577c5915d3744343224158b1e003ba120d6f7
AlphomArt <menelik.wubneh@gmail.com>
2023-07-22 12:29:34 +0200

    test

--- move.c
+++ move.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 23:41:11 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/22 12:20:31 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/22 12:29:26 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ static void	move_down(t_data *data);
 
 int	key_press(int keysym, t_data *data)
 {
-	if (keysym == 65307)
+	if (keysym == XK_Escape)
 		finish_game(data);
-	if (keysym == 119)
+	if (keysym == XK_w)
 		move_top(data);
-	if (keysym == 100)
+	if (keysym == XK_d)
 		move_right(data);
-	if (keysym == 97)
+	if (keysym == XK_a)
 		move_left(data);
-	if (keysym == 115)
+	if (keysym == XK_s)
 		move_down(data);
 	return (0);
 }
--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 23:38:21 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/22 12:25:58 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/22 12:27:06 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	print_window(t_data *data)
 	if (!data->window)
 		return (ft_error("Error : windows creation failed\n", data, 3));
 	mlx_loop_hook(data->mlx_ptr, &print, data);
-	mlx_hook(data->window,KeyRelease, KeyReleaseMask, &key_press, data);
+	mlx_hook(data->window, KeyRelease, KeyReleaseMask, &key_press, data);
 	mlx_hook(data->window, 0, 17, &finish_game, data);
 	mlx_loop(data->mlx_ptr);
 	finish_game(data);
