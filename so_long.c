3ad4f1bb877dd32af18bdcc7d585aa3addf27df3
AlphomArt <menelik.wubneh@gmail.com>
2023-07-22 11:40:17 +0200

    tst

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 20:11:16 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/22 11:39:05 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/22 11:40:06 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ int	main(int argc, char **argv)
 		return ((int)write(2, "Error : data initialisation\n", 28));
 	mapping(data, argv[1]);
 	data->mlx_ptr = mlx_init();
-	if (data->map)
-	{
-		set_pic(data);
+	//if (data->map)
+//	{
+//		set_pic(data);
 //		print_window(data);
 	}
 	//else
-		finish_game(data);
+//		finish_game(data);
 	ft_free(data, 3);
 	return (0);
 }
