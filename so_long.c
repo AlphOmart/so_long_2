f025b7d2d56d1fb43246ef438052d831d347ff9a
AlphomArt <menelik.wubneh@gmail.com>
2023-07-22 11:40:57 +0200

    test

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 20:11:16 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/22 11:40:06 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/22 11:40:49 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(int argc, char **argv)
 //	{
 //		set_pic(data);
 //		print_window(data);
-	}
+	//}
 	//else
 //		finish_game(data);
 	ft_free(data, 3);
