7f0aeac0474f4636a1f5be9929f2ab1661b22f00
AlphomArt <menelik.wubneh@gmail.com>
2023-07-22 12:06:08 +0200

    test

--- set_unset_pic.c
+++ set_unset_pic.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 23:31:51 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/22 12:04:07 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/22 12:05:46 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,12 @@ void	set_pic(t_data *data)
 
 int	finish_game(t_data *data)
 {
-	if (!data->map)
-	{
-		mlx_destroy_image(data->mlx_ptr, data->pic.wall);
-		mlx_destroy_image(data->mlx_ptr, data->pic.path);
-		mlx_destroy_image(data->mlx_ptr, data->pic.item);
-		mlx_destroy_image(data->mlx_ptr, data->pic.player);
-		mlx_destroy_image(data->mlx_ptr, data->pic.exit);
+	mlx_destroy_image(data->mlx_ptr, data->pic.wall);
+	mlx_destroy_image(data->mlx_ptr, data->pic.path);
+	mlx_destroy_image(data->mlx_ptr, data->pic.item);
+	mlx_destroy_image(data->mlx_ptr, data->pic.player);
+	mlx_destroy_image(data->mlx_ptr, data->pic.exit);
 	//	mlx_destroy_window(data->mlx_ptr, data->window);
-	}
 	mlx_destroy_display(data->mlx_ptr);
 	ft_free(data, 3);
 	return (0);
