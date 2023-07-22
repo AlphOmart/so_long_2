e0767ce917bb84c6ea3743b486a425947c31150d
AlphomArt <menelik.wubneh@gmail.com>
2023-07-22 14:19:54 +0200

    test

--- move.c
+++ move.c
@@ -6,15 +6,15 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 23:41:11 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/07/22 14:11:27 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/22 14:19:45 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-//static void	move_top(t_data *data);
-//static void	move_right(t_data *data);
-//static void	move_left(t_data *data);
+static void	move_top(t_data *data);
+static void	move_right(t_data *data);
+static void	move_left(t_data *data);
 static void	move_down(t_data *data, int *pos);
 
 int	key_press(int keysym, t_data *data)
@@ -30,12 +30,12 @@ int	key_press(int keysym, t_data *data)
 		free(pos);
 		finish_game(data);
 	}
-//	if (keysym == XK_w)
-//		move_top(data);
-//	if (keysym == XK_d)
-//		move_right(data);
-//	if (keysym == XK_a)
-//		move_left(data);
+	if (keysym == XK_w)
+		move_top(data);
+	if (keysym == XK_d)
+		move_right(data);
+	if (keysym == XK_a)
+		move_left(data);
 	if (keysym == XK_s)
 	{
 		move_down(data, pos);
@@ -62,107 +62,94 @@ static int	item_left(t_data *data)
 	return (it_nbr);
 }
 
-//static void	move_top(t_data *data)
-//{
-//	int		*pos;
-//
-//	pos = NULL;
-//	pos = get_pos(data, data->map, pos, 'P');
-//	pos[0] = pos[0] / data->pic.height;
-//	pos[1] = pos[1] / data->pic.width;
-//	if (data->map[pos[1] - 1][pos[0]] != data->object->wall)
-//	{
-//		if (!(data->map[pos[1] - 1][pos[0]] == data->object->exit))
-//		{
-//			data->map[pos[1] - 1][pos[0]] = data->object->player;
-//			data->map[pos[1]][pos[0]] = data->object->path;
-//			data->count += 1;
-//			printf("count: %d\n", data->count);
-//		}
-//		else
-//		{
-//			if (item_left(data) == 0)
-//			{
-//				printf("Congrats you won with : %d moves !\n", data->count);
-//				free(pos);
-//				finish_game(data);
-//			}
-//		}
-//	}
-//	free(pos);
-//}
-//
-//void	move_right(t_data *data)
-//{
-//	int		*pos;
-//
-//	pos = NULL;
-//	pos = get_pos(data, data->map, pos, 'P');
-//	pos[0] = pos[0] / data->pic.height;
-//	pos[1] = pos[1] / data->pic.width;
-//	if (data->map[pos[1]][pos[0] + 1] != data->object->wall)
-//	{
-//		if (!(data->map[pos[1]][pos[0] + 1] == data->object->exit))
-//		{
-//			data->map[pos[1]][pos[0] + 1] = data->object->player;
-//			data->map[pos[1]][pos[0]] = data->object->path;
-//			data->count += 1;
-//			printf("count: %d\n", data->count);
-//		}
-//		else
-//		{
-//			if (item_left(data) == 0)
-//			{
-//				printf("Congrats you won with : %d moves !\n", data->count);
-//				free(pos);
-//				finish_game(data);
-//			}
-//		}
-//	}
-//	free(pos);
-//}
-//
-//void	move_left(t_data *data)
-//{
-//	int		*pos;
-//
-//	pos = NULL;
-//	pos = get_pos(data, data->map, pos, 'P');
-//	pos[0] = pos[0] / data->pic.height;
-//	pos[1] = pos[1] / data->pic.width;
-//	if (data->map[pos[1]][pos[0] - 1] != data->object->wall)
-//	{
-//		if (!(data->map[pos[1]][pos[0] - 1] == data->object->exit))
-//		{
-//			data->map[pos[1]][pos[0] - 1] = data->object->player;
-//			data->map[pos[1]][pos[0]] = data->object->path;
-//			data->count += 1;
-//			printf("count: %d\n", data->count);
-//		}
-//		else
-//		{
-//			if (item_left(data) == 0)
-//			{
-//				printf("Congrats you won with : %d moves !\n", data->count);
-//				free(pos);
-//				finish_game(data);
-//			}
-//		}
-//	}
-//	free(pos);
-//}
-//
-void	move_down(t_data *data, int *pos)
+static void	move_top(t_data *data, int *pos)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	x = pos[0];
 	y = pos[1];
-	printf("x = %i\n", x);
-	printf("width= %i\n", data->pic.width);
-	printf("height = %i\n", data->pic.height);
-	printf("y = %i\n", y);
+	if (data->map[y - 1][x] != data->object->wall)
+	{
+		if (!(data->map[y - 1][x] == data->object->exit))
+		{
+			data->map[y - 1][x] = data->object->player;
+			data->map[y][x] = data->object->path;
+			data->count += 1;
+			printf("count: %d\n", data->count);
+		}
+		else
+		{
+			if (item_left(data) == 0)
+			{
+				printf("Congrats you won with : %d moves !\n", data->count);
+				finish_game(data);
+			}
+		}
+	}
+}
+
+void	move_right(t_data *data, int *pos)
+{
+	int	x;
+	int	y;
+
+	x = pos[0];
+	y = pos[1];
+	if (data->map[y][x+ 1] != data->object->wall)
+	{
+		if (!(data->map[y][x + 1] == data->object->exit))
+		{
+			data->map[y][x + 1] = data->object->player;
+			data->map[y][x] = data->object->path;
+			data->count += 1;
+			printf("count: %d\n", data->count);
+		}
+		else
+		{
+			if (item_left(data) == 0)
+			{
+				printf("Congrats you won with : %d moves !\n", data->count);
+				finish_game(data);
+			}
+		}
+	}
+}
+
+void	move_left(t_data *data, int *pos)
+{
+	int	x;
+	int	y;
+
+	x = pos[0];
+	y = pos[1];
+	if (data->map[y][x - 1] != data->object->wall)
+	{
+		if (data->map[y][x - 1] != data->object->exit)
+		{
+			data->map[y][x - 1] = data->object->player;
+			data->map[y][x] = data->object->path;
+			data->count += 1;
+			printf("count: %d\n", data->count);
+		}
+		else
+		{
+			if (item_left(data) == 0)
+			{
+				printf("Congrats you won with : %d moves !\n", data->count);
+				finish_game(data);
+			}
+		}
+	}
+}
+
+void	move_down(t_data *data, int *pos)
+{
+	int	x;
+	int	y;
+
+	x = pos[0];
+	y = pos[1];
 	if (data->map[y + 1][x] != data->object->wall)
 	{
 		if (data->map[y + 1][x] != data->object->exit)
@@ -177,7 +164,6 @@ void	move_down(t_data *data, int *pos)
 			if (item_left(data) == 0)
 			{
 				printf("Congrats you won with : %d moves !\n", data->count);
-				free(pos);
 				finish_game(data);
 			}
 		}
